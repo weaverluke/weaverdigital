c2f41b59b3670afe6993e6d8ad4332504832a3f3
Rich Higgins <hello@iamrichhiggins.com>
2015-12-11 09:43:19 +0000

    outer-container fix w/orientation listener

--- js/production/scripts.js
+++ js/production/scripts.js
@@ -80,6 +80,24 @@ var collapsedContent = {
 	}
 }
 
+var containerHeights = {
+	init:function(){
+		// fix outer container heights
+		var containerHeight = $('.container-outer').height();
+		$('.container-outer').css('height',containerHeight);
+	
+		$(window).on("orientationchange",function(){
+			// re-fix outer container heights
+			$('.container-outer').css('height','');
+		
+			if($('.container-outer').height() != containerHeight){
+				var containerHeight = $('.container-outer').height();
+				$('.container-outer').css('height',containerHeight);
+			}
+		});
+	}
+}
+
 $(function(){
 	
 	if($('#Splash').length){
@@ -94,6 +112,8 @@ $(function(){
 		
 		$('.splash h1, .splash-tagline, .splash .icon-chevron-down').removeClass("hidden");
 	}	
+	
+	containerHeights.init();	
 	
 	/* grunticon Stylesheet Loader | https://github.com/filamentgroup/grunticon | (c) 2012 Scott Jehl, Filament Group, Inc. | MIT license. */
 	window.grunticon=function(e){if(e&&3===e.length){var t=window,n=!(!t.document.createElementNS||!t.document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect||!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")||window.opera&&-1===navigator.userAgent.indexOf("Chrome")),o=function(o){var r=t.document.createElement("link"),a=t.document.getElementsByTagName("script")[0];r.rel="stylesheet",r.href=e[o&&n?0:o?1:2],a.parentNode.insertBefore(r,a)},r=new t.Image;r.onerror=function(){o(!1)},r.onload=function(){o(1===r.width&&1===r.height)},r.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="}};
